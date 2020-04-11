2c4ca9ee425e16e321aa45ee879dccaa30bd3bc6
Piccolomaster336 <rmk336@msn.com>
2020-04-10 20:09:19 -0400

    Add Treenode and AI classes

--- AI.py
+++ AI.py
@@ -1,2 +1,24 @@
 import pydealer
-import gambit
+
+
+class TreeNode:
+
+    def __init__(self, gameBoard, aiHand, cardsPlayed, card, parent):
+        self.table = gameBoard[0]
+        self.singleBuilds = gameBoard[1]
+        self.multiBuilds = gameBoard[2]
+        self.aiHand = aiHand
+        self.cardsPlayed = cardsPlayed
+        self.card = card
+        self.value = 0
+        self.parent = parent
+        self.children = []
+
+
+
+class AI:
+    """Class for managing the AI for Casino.py"""
+    def __init__(self, gameBoard, hand, cardsPlayed):
+        self.gameBoard = gameBoard
+        self.hand = hand
+        self.cardsPlayed = cardsPlayed
