efcf6d59117a613c9b9a07bb94f63de6179583a2
Piccolomaster336 <rmk336@msn.com>
2020-04-14 19:40:45 -0400

    Added logic for findBestCapture

--- AI.py
+++ AI.py
@@ -29,8 +29,99 @@ class AI:
         self.hand = hand
         self.cardsPlayed = cardsPlayed
 
-    def __findBestCapture(self, gameBoard, hand):
-        return ""
+    def __findCombinations(self, table, value):
+        combinations = []
+
+        for i, card in enumerate(table):
+            if self.__getCardValue(card.value) > value:
+                continue
+            elif self.__getCardValue(card.value) == value:
+                combinations.append("/" + pydealer.card.card_abbrev(card.value, card.suit))
+                combinations.extend(self.__findCombinations(table[i+1:], value))
+            elif i < table.size - 1:
+                subCombinations = self.__findCombinations(table[i+1], value - self.__getCardValue(card.value))
+                if subCombinations is not None:
+                    for combo in subCombinations:
+                        combinations.append("/" + pydealer.card.card_abbrev(card.value, card.suit) + "/" + combo)
+
+        if len(combinations) > 0:
+            return combinations
+        else:
+            return None
+
+    def __getPlayValue(self, play):
+        cards = play.split("/")
+        value = 0
+
+        for card in cards:
+            # If it's a play indicator, skip
+            if card[0] == "T" or card[0] == "C" or card[0] == "B":
+                continue
+
+            if card == "10D":
+                value += 10
+            elif card[-1] == "S":
+                if card[0] == "A" or card[0] == "2":
+                    value += 6
+                else:
+                    value += 2
+            elif card[0] == "A":
+                value += 5
+            else:
+                value += 1
+
+
+        return value
+
+    def __findBestCapture(self, gameBoard, hand, forBuilds=False):
+        table = gameBoard[0]
+        
+        combinations = []
+        for card in hand:
+            plays = self.__findCombinations(table, self.__getCardValue(card.value))
+            # For each play, add the card used for the play at the frot of the play string
+            for i, play in enumerate(plays):
+                plays[i] = pydealer.card.card_abbrev(card.value, card.suit) + "/" + plays[i]
+                if self.__getCardValue(card.value) in gameBoard[1]:
+                        plays[i] += "/SB" + str(self.__getCardValue(card.value))
+
+            if not forBuilds:
+                # Check if builds can be captured, add to the end of each combo
+                for i, play in enumerate(plays):
+                    # Add the C to the front. We only want this if the function is called by itself,
+                    # not when it's helping find builds
+                    plays[i] = "C" + plays[i]
+
+                    
+                    if self.__getCardValue(card.value) in gameBoard[2]:
+                        plays[i] += "/MB" + str(self.__getCardValue(card.value))
+                # add to end of plays for just capturing builds
+                if self.__getCardValue(card.value) in gameBoard[1] and self.__getCardValue(card.value) in gameBoard[2]:
+                    plays.append("C/%s/SB%d/MB%d" % (pydealer.card.card_abbrev(card.value, card.suit), self.__getCardValue(card.value), self.__getCardValue(card.value)))
+                elif self.__getCardValue(card.value) in gameBoard[2]:
+                    plays.append("C/"+ pydealer.card.card_abbrev(card.value, card.suit) +"/MB" + str(self.__getCardValue(card.value)))
+            if self.__getCardValue(card.value) in gameBoard[1]:
+                plays.append("C/"+ pydealer.card.card_abbrev(card.value, card.suit) +"/SB" + str(self.__getCardValue(card.value)))
+
+            combinations.extend(plays)
+        
+        # If no combinations found, trail first card of hand
+        if len(combinations) == 0:
+            return "T/%s/0" % (pydealer.card.card_abbrev(hand[0].value, hand[0].suit))
+
+        # otherwise, figure out the best combo and return that
+        index = 0
+        maximum = 0
+        for i, play in enumerate(combinations):
+            value = self.__getPlayValue(play)
+            if value > maximum:
+                maximum = value
+                index = i
+        
+        if not forBuilds:
+            return combinations[index] + "/" + str(maximum)
+        else:
+            return combinations[index]
 
     def __findBestBuild(self, gameBoard, hand):
         return ""
@@ -60,6 +151,20 @@ class AI:
             return "K"
         else:
             return "Something went wrong"
+
+    def __getCardValue(self, value):
+        if value == "Ace" or value == "ace" or value == "A" or value == "a":
+            value = 1
+        elif value == "Jack" or value == "jack" or value == "J" or value == "j":
+            value = 11
+        elif value == "Queen" or value == "queen" or value == "Q" or value == "q":
+            value = 12
+        elif value == "King" or value == "king" or value == "K" or value == "k":
+            value = 13
+        else:
+            value = int(value)
+
+        return value
 
     def __buildTree(self, parent):
         # Find the best capture and build given the current gamestate
